7c7fcad32f23fbe41fd446cf953c4f15a2aaa7b7
Erik-Romkema <227160@edu.rocfriesepoort.nl>
2023-06-14 22:15:43 +0200

    File Structure & Login Page

---
+++ login.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="login/css/inlog.css">
+        <title>Breman - Login</title>
+        <link rel="icon" href="login/assets/img/icon.png" type="image/x-icon">
+        <script src="login/JS/JavaScript.js"></script>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <div class="headersection">
+            <div class="Header">
+                <div class="HL1">
+                    <div class="HL2">
+                        <div class="HB1"></div>
+                        <div class="HB2">
+                            <img src="login/assets/img/logo.png">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <br>
+
+
+        <div class="contentsection">
+            <div id="inlog01">
+                <input placeholder="Naam" type="text" name="naam" />
+                <input placeholder="Wachtwoord" type="password" name="password" />
+                <input type="submit" name="inloggen" value="Inloggen" id="butnhover"/>
+            </div>
+        </div>
+    </body>
+</html>
